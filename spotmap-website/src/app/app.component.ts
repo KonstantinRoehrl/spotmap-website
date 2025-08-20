7057fb428cf5189ffe94ce9cb0036a8cd6c8204e
Konstantin Röhrl <konstantin.roehrl@tuwien.ac.at>
2025-08-20 14:47:07 +0200

    First rudimentary version with map

--- spotmap-website/src/app/app.component.ts
+++ spotmap-website/src/app/app.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MapContainerComponent } from './modules/map-container/map-container.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MapContainerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'spotmap-website';
+
+  protected mapsLink = "https://www.google.com/maps/d/u/0/embed?mid=13c9hk1PqIRE5jgjTAr1pf4sP_9GNiIg&ehbc=2E312F";
 }
