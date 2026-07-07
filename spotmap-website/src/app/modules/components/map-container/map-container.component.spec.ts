f4d5dbcf2377c91417d28110f3c8a4e6388c3dc1
KonstantinRoehrl <konstantinroehrl@yahoo.com>
2026-07-07 22:43:26 +0200

    fix: derive MapContainer safeUrl via computed; set required input in spec

--- spotmap-website/src/app/modules/components/map-container/map-container.component.spec.ts
+++ spotmap-website/src/app/modules/components/map-container/map-container.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { CityEnum } from '../../../models/enums/map-enum';
 import { MapContainerComponent } from './map-container.component';
 
 describe('MapContainerComponent', () => {
@@ -8,16 +9,20 @@ describe('MapContainerComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MapContainerComponent]
-    })
-    .compileComponents();
+      imports: [MapContainerComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(MapContainerComponent);
+    fixture.componentRef.setInput('city', CityEnum.Vienna);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('exposes a sanitized url for the selected city', () => {
+    expect(component.safeUrl()).toBeTruthy();
   });
 });
--- spotmap-website/src/app/modules/components/map-container/map-container.component.ts
+++ spotmap-website/src/app/modules/components/map-container/map-container.component.ts
@@ -1,40 +1,28 @@
-import { Component, effect, input, OnInit, signal } from '@angular/core';
+import { Component, computed, input, signal } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { SUPPORTED_CITIES } from '../../../models/enums/config';
 import { CityEnum } from '../../../models/enums/map-enum';
 import { LoadingBarComponent } from '../loading-bar/loading-bar.component';
-
 
 @Component({
   selector: 'app-map-container',
   standalone: true,
   imports: [LoadingBarComponent],
   templateUrl: './map-container.component.html',
-  styleUrl: './map-container.component.css'
+  styleUrl: './map-container.component.css',
 })
-export class MapContainerComponent implements OnInit {
-
+export class MapContainerComponent {
   city = input.required<CityEnum>();
 
-  safeUrl?: SafeResourceUrl;
-
-  constructor(private sanitizer: DomSanitizer) {
-    // Reactively sanitize whenever mapLink changes
-    effect(() => {
-      const url = SUPPORTED_CITIES[this.city()].mapLink;
-      this.safeUrl = this.sanitizer.bypassSecurityTrustResourceUrl(url);
-    });
-  }
+  readonly safeUrl = computed<SafeResourceUrl>(() =>
+    this.sanitizer.bypassSecurityTrustResourceUrl(SUPPORTED_CITIES[this.city()].mapLink),
+  );
 
   protected iframeLoaded = signal(false);
 
+  constructor(private sanitizer: DomSanitizer) {}
+
   onIframeLoad() {
-    setTimeout(() => {
-      this.iframeLoaded.set(true);
-    }, 700);
+    setTimeout(() => this.iframeLoaded.set(true), 700);
   }
-
-  ngOnInit(): void {
-  }
-
 }
